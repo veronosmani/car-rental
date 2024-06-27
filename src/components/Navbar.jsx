6b9689a361a7a8846ddcffe571b623b6de53f86e
veronosmani <veron.osmani@gmail.com>
2024-06-27 15:46:11 +0200

    modified:   src/components/Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,49 @@
+const NavLinks = [
+  {
+    id: "1",
+    name: "HOME",
+    link: "/",
+  },
+  {
+    id: "2",
+    name: "CARS",
+    link: "/#cars",
+  },
+  {
+    id: "3",
+    name: "ABOUT",
+    link: "/#about",
+  },
+  {
+    id: "4",
+    name: "BOOKING",
+    link: "/#booking",
+  },
+];
+
 const Navbar = () => {
   return (
-    <nav>
-        <div className="container">
-            <div className="flex">
-                <div>
-                    <h1>Navbar</h1>
-                </div>
-                <div>
-                    <ul>
-                        <li>
-                            <a href="#">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">About</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+    <nav className="shadow-sm ">
+      <div className="container">
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="text-xl font-bold">Car Rental</h1>
+          </div>
+          <div>
+            <ul className="flex items-center gap-6 ">
+              {NavLinks.map((data) => (
+                <li key={data.id}>
+                  <a className="inline-block py-4" href={data.link}>
+                    {data.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
