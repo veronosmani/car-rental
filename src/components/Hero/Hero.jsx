e6406458e7a0bf92ca1479f253b9ed2002c8a2ec
veronosmani <veron.osmani@gmail.com>
2024-06-27 16:54:04 +0200

    modified:   src/components/Hero/Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,26 @@
+import carPng from "../../assets/car.png";
+import car1Png from "../../assets/car1.png";
+
 const Hero = () => {
   return (
     <div className="dark:bg-black dark:text-white light:bg-white light:text-black duration-300">
-        <div className="container min-h-[620px]">
-            <div className="grid place-items-center grid-cols-1 sm:grid-cols-2">
-                <div>
-                    <img src={carPng} alt="" />
-                </div>
-                <div></div>
-            </div>
+      <div className="container min-h-[620px]">
+        <div className="grid place-items-center grid-cols-1 sm:grid-cols-2">
+          <div className="order-1 sm:order-2">
+            <img src={carPng} alt="" />
+          </div>
+          <div className="order-1 sm:order-2">
+            <p>Effortless</p>
+            <h1>Car Rental</h1>
+            <p>
+              Lorem ipsum dolor sit amet consece adispinfs elit. Inasdnio
+              sndjadj voulptate mollitia. Nulla, disgnsissmos consqetuneur!
+            </p>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
