3f02ef5bf94c8ddc96aeb88a498916e90c44c1ed
veronosmani <veron.osmani@gmail.com>
2024-06-27 16:42:30 +0200

    modified:   src/components/Hero/Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,15 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="dark:bg-black dark:text-white light:bg-white light:text-black duration-300">
+        <div className="container min-h-[620px]">
+            <div className="grid place-items-center grid-cols-1 sm:grid-cols-2">
+                <div>
+                    <img src={carPng} alt="" />
+                </div>
+                <div></div>
+            </div>
+        </div>
+    </div>
   )
 }
 
