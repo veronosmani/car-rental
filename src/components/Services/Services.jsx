37aee4e5d3793becaa3b5217aebb2788986b97d6
veronosmani <veron.osmani@gmail.com>
2024-06-28 11:24:52 +0200

    modified:   src/components/Services/Services.jsx

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { FaCameraRetro } from "react-icons/fa";
 import { GiNotebook } from "react-icons/gi";
 import { SlNote } from "react-icons/sl";
@@ -9,7 +10,7 @@ const skillsData = [
       <FaCameraRetro className="text-5xl text-primary group-hover:text-black duration-300" />
     ),
     link: "#",
-    description: "Lorem ipsum dolor sit amet consectetur, adipiscing elit.",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
     aosDelay: "0",
   },
   {
@@ -18,46 +19,56 @@ const skillsData = [
       <GiNotebook className="text-5xl text-primary group-hover:text-black duration-300" />
     ),
     link: "#",
-    description: "Lorem ipsum dolor sit amet consectetur, adipiscing elit.",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
     aosDelay: "500",
   },
   {
     name: "Experience Drivers",
     icon: (
-      <SlNote className="text-5xl text-primary group-hover:text-black duration-300" />
+      <SlNote className="text-5xl text-primary group-hover:text-black duration-500" />
     ),
     link: "#",
-    description: "Lorem ipsum dolor sit amet consectetur, adipiscing elit.",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
     aosDelay: "1000",
   },
 ];
-
 const Services = () => {
   return (
-    <div className="py-14 dark:bg-black dark:text-white sm:min-h-[600px] sm:grid sm:place-items-center">
-      <div className="container">
-        <div className="pb-12">
-          <h1 className="text-3xl font-semibold text-center font-serif sm:text-4xl ">
-            Why Choose Us
-          </h1>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {skillsData.map((skills) => (
-            <div
-              key={skills.name}
+    <>
+      <span id="about"></span>
+      <div className="dark:bg-black dark:text-white py-14 sm:min-h-[600px] sm:grid sm:place-items-center">
+        <div className="container">
+          <div className="pb-12">
+            <h1
               data-aos="fade-up"
-              data-aos-delay={skills.aosDelay}
-              className="card text-center group-space-y-3 sm:space-y-6 p-4 sm:py-16 bg-dar hover:bg-primary duration-300 text-white hover:text-black rounded-lg"
+              className="text-3xl font-semibold text-center sm:text-4xl font-serif"
             >
-              <div className="grid place-items-center">{skills.icon}</div>
-              <h1>{skills.name}</h1>
-              <p>{skills.description}</p>
-              <a href={skills.link}>Learn More</a>
-            </div>
-          ))}
+              Why Choose Us
+            </h1>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            {skillsData.map((skill) => (
+              <div
+                key={skill.name}
+                data-aos="fade-up"
+                data-aos-delay={skill.aosDelay}
+                className="card text-center group space-y-3 sm:space-y-6 p-4 sm:py-16 bg-dark  hover:bg-primary duration-300 text-white hover:text-black rounded-lg"
+              >
+                <div className="grid place-items-center">{skill.icon}</div>
+                <h1 className="text-2xl font-bold">{skill.name}</h1>
+                <p>{skill.description}</p>
+                <a
+                  href={skill.link}
+                  className="inline-block text-lg font-semibold py-3 text-primary group-hover:text-black duration-300"
+                >
+                  Learn more
+                </a>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
