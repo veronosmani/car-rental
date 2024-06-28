50e83ae57509a9d037ebeadf90a1242b7d02a1fa
veronosmani <veron.osmani@gmail.com>
2024-06-28 11:24:00 +0200

    modified:   src/App.jsx 	modified:   src/components/Services/Services.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero/Hero";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import About from "./components/About/About";
+import Services from "./components/Services/Services";
 
 const App = () => {
   const [theme, setTheme] = useState(
@@ -37,6 +38,7 @@ const App = () => {
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero theme={theme} />
       <About />
+      <Services />
     </div>
   );
 };
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,7 +1,64 @@
+import { FaCameraRetro } from "react-icons/fa";
+import { GiNotebook } from "react-icons/gi";
+import { SlNote } from "react-icons/sl";
+
+const skillsData = [
+  {
+    name: "Best Price",
+    icon: (
+      <FaCameraRetro className="text-5xl text-primary group-hover:text-black duration-300" />
+    ),
+    link: "#",
+    description: "Lorem ipsum dolor sit amet consectetur, adipiscing elit.",
+    aosDelay: "0",
+  },
+  {
+    name: "Fast and Safe",
+    icon: (
+      <GiNotebook className="text-5xl text-primary group-hover:text-black duration-300" />
+    ),
+    link: "#",
+    description: "Lorem ipsum dolor sit amet consectetur, adipiscing elit.",
+    aosDelay: "500",
+  },
+  {
+    name: "Experience Drivers",
+    icon: (
+      <SlNote className="text-5xl text-primary group-hover:text-black duration-300" />
+    ),
+    link: "#",
+    description: "Lorem ipsum dolor sit amet consectetur, adipiscing elit.",
+    aosDelay: "1000",
+  },
+];
+
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <div className="py-14 dark:bg-black dark:text-white sm:min-h-[600px] sm:grid sm:place-items-center">
+      <div className="container">
+        <div className="pb-12">
+          <h1 className="text-3xl font-semibold text-center font-serif sm:text-4xl ">
+            Why Choose Us
+          </h1>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {skillsData.map((skills) => (
+            <div
+              key={skills.name}
+              data-aos="fade-up"
+              data-aos-delay={skills.aosDelay}
+              className="card text-center group-space-y-3 sm:space-y-6 p-4 sm:py-16 bg-dar hover:bg-primary duration-300 text-white hover:text-black rounded-lg"
+            >
+              <div className="grid place-items-center">{skills.icon}</div>
+              <h1>{skills.name}</h1>
+              <p>{skills.description}</p>
+              <a href={skills.link}>Learn More</a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Services
+export default Services;
