5e644407c00e5c1be7abce1005334f43f7334af7
veronosmani <veron.osmani@gmail.com>
2024-06-28 09:52:48 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	new file:   src/components/About/About.jsx 	modified:   src/components/Hero/Hero.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,44 @@
-import { useEffect, useState } from "react"
-import Navbar from "./components/Navbar/Navbar"
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import About from "./components/About/About";
 
 const App = () => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
 
-  const [theme, setTheme] = useState(localStorage.getItem("theme")? localStorage.getItem("theme"):"light")
-
-  const element= document.documentElement;
+  const element = document.documentElement;
   useEffect(() => {
-    if(theme === "dark"){
+    if (theme === "dark") {
       element.classList.add("dark");
-      localStorage.setItem("theme","dark");
-    }else{
+      localStorage.setItem("theme", "dark");
+    } else {
       element.classList.remove("dark");
-      localStorage.setItem("theme","light");
+      localStorage.setItem("theme", "light");
     }
-  },[theme])
+  }, [theme]);
+
+  // AOS Initialization
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
 
   return (
-   <div>
-    <Navbar theme={theme} setTheme={setTheme}/>
-    <Hero theme={theme}/>   
-   </div>
-  )
-}
+    <div>
+      <Navbar theme={theme} setTheme={setTheme} />
+      <Hero theme={theme} />
+      <About />
+    </div>
+  );
+};
 
-export default App
+export default App;
