1fab954563de73d19b6185542e8d6df9ec5a7f0a
veronosmani <veron.osmani@gmail.com>
2024-07-04 09:16:03 +0200

    new file:   nasa-api-react     new file:   src/components/AppStoreBanner/AppStoreBanner.jsx 	modified:   src/components/Testimonial/Testimonial.jsx

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -37,7 +37,7 @@ const Testimonial = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 text-black dark:text-white">
           {testimonialData.map((data) => {
             return (
-              <div key={data.name}>
+              <div data-aos="fade-up" data-aos-delay={data.aosDelay} className="card text-center group space-y-3 sm:space-y-6 p-4 bg-gray-100 dark:bg-white/20 sm:py-12 duration-300 rounded-lg" key={data.name}>
                 <div className="grid place-items-center">
                   <img
                     src="https://picsum.photos/id/237/200"
